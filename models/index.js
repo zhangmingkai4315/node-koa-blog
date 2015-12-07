6c3c6fade5bdbaed53cc22e547c4a9c915a11700
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2015-12-07 23:47:01 +0800

    lib interface of Models
    lib interface of Models

---
+++ models/index.js
@@ -0,0 +1,11 @@
+var mongoose=require('mongoose');
+var config=require('config-lite').mongodb;
+mongoose.connect(config.url,function (err) {
+  if(err){
+    console.error('Connect to %s error: ',config.url,err.message);
+      process.exit(1);
+  }
+});
+exports.User=require('./user');
+exports.Topic=require('./topic');
+exports.Comment=require('./comment');
