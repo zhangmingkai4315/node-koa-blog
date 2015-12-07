df79020a3f35e7983a2a307251c9c18b6546eea2
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2015-12-07 22:32:34 +0800

    app.js
    生成程序框架

---
+++ app.js
@@ -0,0 +1,38 @@
+var koa = require('koa');
+var logger = require('koa-logger');
+var bodyParser=require('koa-bodyparser');
+var staticCache=require('koa-static-cache');
+var errorHandler=require('koa-errorhandler');
+var session=require('koa-generic-session');
+var MongoStore=require('koa-generic-session-mongo');
+var flash=require('koa-flash');
+var gzip=require('koa-gzip');
+var scheme=require('koa-scheme');
+var router=require('koa-frouter');
+var render=require('co-ejs');
+var config=require('config-lite');
+
+var merge=require('merge-descriptors');
+var core=require('./lib/core');
+var renderConf=require(config.renderConf);
+merge(renderConf.locals||{},core,false);
+
+app.keys=[renderConf.locals.$app.name];
+
+app.use(errorHandler());
+app.use(bodyParser());
+app.use(staticCache(config.staticCacheConf));
+app.use(logger());
+app.use(session({
+  store:new MongoStore(config.mongodb)
+}));
+app.use(flash());
+
+app.use(scheme(config.schemeConf));
+app.use(routerCache(app,config.routerCacheConf));
+app.use(gzip());
+app.use(render(app,renderConf));
+app.use(router(app,config.routerConf));
+app.listen(config.port,function () {
+  console.log("Server listen on :" +config.port);
+});
---
+++ config/default.js
@@ -0,0 +1,19 @@
+var path = require('path');
+module.exports={
+  port:process.env.PORT||3000,
+  mongodb:{
+    url:'mongodb://127.0.0.1:27017/koablog'
+  },
+  schemeConf:path.join(__dirname,'./default.scheme'),
+  staticCacheConf:path.join(__dirname,'../theme/publices'),
+  renderConf:path.join(__dirname,'../theme/config'),
+  routerConf:'routes',
+  routerCacheConf:{
+    '/':{
+      expire:10*1000,
+      condition:function () {
+        return !this.session||!this.session.user;
+      }
+    }
+  }
+}
